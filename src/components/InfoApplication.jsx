3fa50e669f494ef28d88dfea55838827e81cd910
KutuzovSergey <SergeiPasekin@yandex.ru>
2023-04-09 15:12:50 +0300

    adaptability of the info block

--- src/components/InfoApplication.jsx
+++ src/components/InfoApplication.jsx
@@ -1,21 +1,41 @@
 import React from "react";
-import '../styles/componentStyles/InfoApplication.scss';
 import imgFon from '../images/home/imgFon.jpg';
+import imgFon_second from '../images/home/imgFon_second.jpg';
+
+import '../styles/componentStyles/InfoApplication.scss';
 
 const InfoApplication = () => {
+
     return (
         <div className="info">
             <div className="info__title">
                 <span>О ресурсе</span>
             </div>
-            <div className="info__block">
-                <div className="info__text">
-                    <p>Для того, чтобы повысить доходность сделок, трейдеры должны использовать все доступные им средства для снижения риска.</p>
-                    <p>Обычно для этой цели проводят анализ выбранных активов с применением методов технического и фундаментального анализа.</p>
-                    <p>Нашь ресурс предоставляет полноценный набор инструментов для тчательного анализа рынка и отдельных криптовалют.</p>
+            <div className="info__wrapper">
+                <div className="info__block info__block-reverse">
+                    <div className="info__text">
+                        <div className="info__text__block">
+                            <p>Для того, чтобы повысить доходность сделок, трейдеры должны использовать все доступные им средства для снижения риска.</p>
+                            <p>Обычно для этой цели проводят анализ выбранных активов с применением методов технического и фундаментального анализа.</p>
+                            <p>Нашь ресурс предоставляет полноценный набор инструментов для тчательного анализа рынка и отдельных криптовалют.</p>
+                        </div>
+                    </div>
+                    <div className="info__picture">
+                        <img src={imgFon} alt="" />
+                    </div>
                 </div>
-                <div className="info__picture">
-                    <img src={imgFon} alt="background picture" />
+                <div className="info__block">
+                    <div className="info__picture">
+                        <img src={imgFon_second} alt="" />
+                    </div>
+                    <div className="info__text">
+                        <div className="info__text__block">
+                            <p>Для того, чтобы повысить доходность сделок, трейдеры должны использовать все доступные им средства для снижения риска.</p>
+                            <p>Обычно для этой цели проводят анализ выбранных активов с применением методов технического и фундаментального анализа.</p>
+                            <p>Нашь ресурс предоставляет полноценный набор инструментов для тчательного анализа рынка и отдельных криптовалют.</p>
+                        </div>
+                        
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/menus/AdaptiveMenu/AdaptiveMenu.jsx
+++ src/components/menus/AdaptiveMenu/AdaptiveMenu.jsx
@@ -1,16 +1,14 @@
 import React, { useContext } from "react";
 import { Link } from 'react-router-dom';
 import { AutchContext } from "../../../context";
+import home_page_icon from "../../../images/icon/home_page_icon.png";
+import person_icon from "../../../images/icon/person_icon.png";
+import coin_icon from "../../../images/icon/coin_icon.png";
 
 import cl from "./AdaptiveMenu.module.scss";
 
 const AdaptiveMenu = () =>{
     const {menuAdaptive, setMenuAdaptive} = useContext(AutchContext);
-
-    // const closeMenuAdaptive = () =>{
-    //     console.log('работает')
-    //     setMenuAdaptive(false)
-    // }
 
     const classMenu = [cl.menu]
 
@@ -24,19 +22,34 @@ const AdaptiveMenu = () =>{
             <nav className={cl.menu__content}>
                 <ul className={cl.menu__block}>
                     <li className={cl.menu__item}>
-                        <Link to="/" onClick={() => {setMenuAdaptive(false)}} >Главная</Link>
+                        <Link to="/" onClick={() => {setMenuAdaptive(false)}} >
+                            <div className={cl.menu__item__icon}>
+                                <img src={home_page_icon} alt="" />
+                            </div>
+                            <span className={cl.menu__item__text}>Главная</span> 
+                        </Link>
                     </li>
                     <li className={cl.menu__item}>
                         <hr className={cl.menu__line}/>
                     </li>
                     <li className={cl.menu__item}>
-                        <Link to="/currences" onClick={() => {setMenuAdaptive(false)}} >Монеты</Link>
+                        <Link to="/currences" onClick={() => {setMenuAdaptive(false)}} >
+                            <div className={cl.menu__item__icon}>
+                                <img src={coin_icon} alt="" />
+                            </div>
+                            <span className={cl.menu__item__text}>Монеты</span>
+                        </Link>
                     </li>
                     <li className={cl.menu__item}>
                         <hr className={cl.menu__line}/>
                     </li>
                     <li className={cl.menu__item}>
-                        <Link to="/user-account" onClick={() => {setMenuAdaptive(false)}} >Кабинет</Link>
+                        <Link to="/user-account" onClick={() => {setMenuAdaptive(false)}} >
+                            <div className={cl.menu__item__icon}>
+                                <img src={person_icon} alt="" />
+                            </div>
+                            <span className={cl.menu__item__text}>Кабинет</span>
+                        </Link>
                     </li>
                 </ul>
             </nav>
