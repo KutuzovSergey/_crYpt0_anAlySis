201ebc3d32316a4d720dbc1af92a0b8f27a19f32
KutuzovSergey <SergeiPasekin@yandex.ru>
2023-04-20 15:34:39 +0300

    validation hook in the registration form

--- src/components/ModalLoginForm.jsx
+++ src/components/ModalLoginForm.jsx
@@ -10,7 +10,7 @@ const ModalLoginForm = () => {
 
     const [form, setForm] = useState({
         login: '',
-        pasword: ''
+        password: ''
     });
 
     // const useValidation = (value, validations) =>{
@@ -34,12 +34,12 @@ const ModalLoginForm = () => {
 
     const [dirty, setDirty] = useState({
         loginDirty: false,
-        paswordDirty: false
+        passwordDirty: false
     })
 
     const [error, setError] = useState({
         loginError: 'Логин не моет быть пустым',
-        paswordError: 'Пароль не может быть пустым'
+        passwordError: 'Пароль не может быть пустым'
     });
 
     const [formValid, setFormValid] = useState(false);
@@ -50,7 +50,7 @@ const ModalLoginForm = () => {
         const reEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
 
         if(!reEmail.test(String(e.target.value).toLowerCase())){
-            setError({...error, loginError: 'Логин указан не коректно, введите телефон или email'});
+            setError({...error, loginError: 'Логин указан не коректно, введите email'});
             if (!e.target.value) {
                 setError({...error, loginError: 'Пароль не может быть пустым'});
             }
@@ -60,15 +60,15 @@ const ModalLoginForm = () => {
     }
 
     const paswordChange = (e) =>{
-        setForm({...form, pasword: e.target.value});
+        setForm({...form, password: e.target.value});
 
-        if (form.pasword.length < 6) {
-            setError({...error, paswordError: 'Пороль не может быть меньше 6 символов'});
+        if (form.password.length < 6) {
+            setError({...error, passwordError: 'Пороль не может быть меньше 6 символов'});
             if (!e.target.value) {
-                setError({...error, paswordError: 'Пароль не может быть пустым'});
+                setError({...error, passwordError: 'Пароль не может быть пустым'});
             }
         } else {
-            setError({...error, paswordError: ''});
+            setError({...error, passwordError: ''});
         }
     }
 
@@ -84,15 +84,15 @@ const ModalLoginForm = () => {
             case 'login':
                 setDirty({...dirty, loginDirty: true})
                 break;
-            case 'pasword':
-                setDirty({...dirty, paswordDirty: true})
+            case 'password':
+                setDirty({...dirty, passwordDirty: true})
                 break;
                 default: 
         }
     }
 
     useEffect( () => {
-        if (error.loginError || error.paswordError) {
+        if (error.loginError || error.passwordError) {
             setFormValid(false);
         } else {
             setFormValid(true);
@@ -102,8 +102,8 @@ const ModalLoginForm = () => {
 
     useEffect( () => {
         if(modalLogin){
-            setForm({...form, login: '', pasword: ''});
-            setDirty({...dirty, loginDirty: false, paswordDirty: false})
+            setForm({...form, login: '', password: ''});
+            setDirty({...dirty, loginDirty: false, passwordDirty: false})
         }
     }, [modalLogin]);
 
@@ -124,18 +124,18 @@ const ModalLoginForm = () => {
                     autoComplete='email' />
             </div>
             <div className="form__input">
-                {(dirty.paswordDirty && error.paswordError) && <ErrorForm>{error.paswordError}</ErrorForm>}
+                {(dirty.passwordDirty && error.passwordError) && <ErrorForm>{error.passwordError}</ErrorForm>}
                 <MyInput 
-                    type='pasword'
-                    name='pasword'
-                    value={form.pasword}
+                    type='password'
+                    name='password'
+                    value={form.password}
                     onChange={ (e) => paswordChange(e)}
                     onBlur={ (e) => blurHandler(e)}
                     placeholder='пароль'
                     autoComplete='current-password'/>
             </div>
             <div className="form__button">
-                <MyButton disabled={!formValid} >войти</MyButton>
+                <MyButton disabled={!formValid}>войти</MyButton>
             </div>
         </form>
     )
--- src/components/ModalRegistrForm.jsx
+++ src/components/ModalRegistrForm.jsx
@@ -1,23 +1,103 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import MyInput from "./UI/MyInput/MyInput";
 import MyButton from "./UI/MyButton/MyButton";
 import user from "../images/user/user.png";
 import camera from "../images/icon/camera.svg";
 import upload from "../images/icon/upload.svg";
 import { AutchContext } from "../context";
+import ErrorForm from "./UI/ErrorForm/ErrorForm";
+
+
+const useValidation = (value, validations) =>{
+    const [isEmpty, setIsEmpty] = useState(true);
+    const [minLengthError, setMinLengthError] = useState(false);
+    const [battonValid, setBattinValid] = useState(false);
+    const [emailError, setEmailError] = useState(false);
+    const [repeatPassword, setRepeatPassword] = useState(false);
+    const reEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+    const [errorText, setErrorText] = useState('');
+
+    useEffect( () => {
+        for (const validation in validations){
+            switch (validation) {
+                case 'minLength':
+                    value.length < validations[validation] ? setMinLengthError(true) : setMinLengthError(false);
+                    break;
+                case 'isEmpty':
+                    value ? setIsEmpty(false) : setIsEmpty(true);
+                    break;
+                case 'email':
+                    !reEmail.test(String(value).toLowerCase()) ? setEmailError(true) : setEmailError(false);
+                    break;
+                case 'secondPassword':
+                    console.log(validations.secondPassword);
+                    value === validations.secondPassword ? setRepeatPassword(false) : setRepeatPassword(true);
+                    break;
+                default:
+            }
+        }
+    }, [value]);
+
+    useEffect( () => {
+        if (isEmpty || minLengthError) {
+            setBattinValid(true);
+        } else {
+            setBattinValid(false);
+        }
+
+    }, [isEmpty, minLengthError]);
+
+    return {
+        isEmpty,
+        minLengthError,
+        battonValid,
+        emailError,
+        errorText,
+        repeatPassword,
+    }
+}
+
+const useInput = (initialValue, validations) => {
+    const [ value, setValue ] = useState(initialValue);
+    const [ isDirty, setIsDirty] = useState(false);
+    const valid = useValidation(value, validations);
+
+    const onChange = (e) =>{
+        setValue(e.target.value);
+    }
+
+    const onBlur = () => {
+        setIsDirty(true)
+    }
+
+    return {
+        value,
+        onChange,
+        onBlur,
+        ...valid,
+        isDirty,
+    }
+}
 
 const ModalRegistrForm = () => {
     const {setIsAuth, setModalRegistr} = useContext(AutchContext);
 
-    const registration = event => {
-        event.preventDefault();
+    const name = useInput('', {isEmpty: true, minLength: 2,});
+    const email = useInput('', {isEmpty: true, email: true});
+    const password = useInput('', {isEmpty: true, minLength: 8});
+    const repeatPassword = useInput('', {isEmpty: true, secondPassword: password.value});
+    const phone = useInput('', {isEmpty: true,})
+
+    const registration = (e) => {
+        e.preventDefault();
         setIsAuth(true);
         localStorage.isAuth = true;
         setModalRegistr(false);
     }
 
     return (
-        <form className="form" onSubmit={registration}> 
+        <form className="form" onSubmit={ (e) => registration(e)}> 
             <div className="form__title">
                 <span className="form__title_text">Зарегистрироваться</span>
             </div>
@@ -27,39 +107,71 @@ const ModalRegistrForm = () => {
                         <img src={user} alt="user" />
                     </div>
                     <div className="form__block__get__img__button">
-                        <MyButton><img src={camera} alt="camera" /></MyButton>
-                        <MyButton><img src={upload} alt="upload" /></MyButton>
+                        <MyButton>
+                            <img src={camera} alt="camera" />
+                        </MyButton>
+                        <MyButton>
+                            <img src={upload} alt="upload" />
+                        </MyButton>
                     </div>
                 </div>
                 <div className="form__block__input">
                     <div className="form__input">
+                        {(name.isDirty && name.isEmpty) && <ErrorForm>Поле не может быть пустым</ErrorForm>}
                         <MyInput
                             type='text'
                             placeholder='имя'
-                            autoComplete='name'/>
+                            autoComplete='name'
+                            onBlur={ (e) => name.onBlur(e)}
+                            onChange={ (e) => name.onChange(e)} />
                     </div>
                     <div className="form__input">
+                        {(password.isDirty && password.isEmpty) && <ErrorForm>Поле не может быть пустым</ErrorForm>}
+                        {(password.minLengthError && !password.isEmpty) && <ErrorForm>Пороль должен быть больше 8 символов</ErrorForm>}
                         <MyInput 
                             type='password'
                             placeholder='пароль'
-                            autoComplete='new-password'/>
+                            autoComplete='new-password'
+                            value={password.value}
+                            onBlur={ (e) => password.onBlur(e)}
+                            onChange={ (e) => password.onChange(e)} />
                     </div>
                     <div className="form__input">
+                        {(repeatPassword.isDirty && repeatPassword.isEmpty) && <ErrorForm>Поле не может быть пустым</ErrorForm>}
+                        {(repeatPassword.repeatPassword && !repeatPassword.isEmpty) && <ErrorForm>Пароли не совпадают</ErrorForm>}
                         <MyInput 
                             type='password'
                             autoComplete='new-password'
-                            placeholder='повторите пароль'/>
+                            placeholder='повторите пароль' 
+                            onBlur={ (e) => repeatPassword.onBlur(e)}
+                            onChange={ (e) => repeatPassword.onChange(e)} />
                     </div>
                 </div>
             </div>
             <div className="form__input">
+                {(email.isDirty && email.isEmpty) && <ErrorForm>Поле не может быть пустым</ErrorForm>}
+                {(email.emailError && !email.isEmpty) && <ErrorForm>Email указан не коректно</ErrorForm>}
                 <MyInput 
-                    type='text'
+                    type='email'
+                    name='email'
                     autoComplete='email'
-                    placeholder='почта / телефон'/>
+                    placeholder='почта'
+                    value={email.value} 
+                    onBlur={(e) => email.onBlur(e)}
+                    onChange={ (e) => email.onChange(e)} />
+            </div>
+            <div className="form__input">
+                {(phone.isDirty && phone.isEmpty) && <ErrorForm>Поле не может быть пустым</ErrorForm>}
+                <MyInput 
+                    type='tel' 
+                    name='phone'
+                    autoComplete='phone'
+                    placeholder='телефон'
+                    onBlur={(e) => phone.onBlur(e)}
+                    onChange={ (e) => phone.onChange(e)} />
             </div>
             <div className="form__button">
-                <MyButton>войти</MyButton>
+                <MyButton disabled={!email.battonValid || !password.battonValid || !phone.battonValid || name.battonValid}>войти</MyButton>
             </div>
         </form>
     )
